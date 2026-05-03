dc10716559b8f62285915c913a886afb634a4321
fradere <kulikovaos2845@gmail.com>
2026-05-03 18:09:18 +0500

    update binaries and assembly info for Ghost_house_game

--- Ghost_house_game/Ghost_house_game/obj/Debug/net9.0/Ghost_house_game.AssemblyInfo.cs
+++ Ghost_house_game/Ghost_house_game/obj/Debug/net9.0/Ghost_house_game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ghost_house_game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05790a650484346c7fe994b798c9feb6292f41c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82907e3413b7848450ee3aa0c13fc5b6becca51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ghost_house_game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ghost_house_game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
