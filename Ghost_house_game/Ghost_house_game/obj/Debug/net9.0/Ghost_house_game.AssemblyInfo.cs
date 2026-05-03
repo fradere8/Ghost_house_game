4ed69395f74d655827ac14046204c0846f58fe26
fradere <kulikovaos2845@gmail.com>
2026-05-03 15:32:25 +0500

    initialize proj

--- Ghost_house_game/Ghost_house_game/obj/Debug/net9.0/Ghost_house_game.AssemblyInfo.cs
+++ Ghost_house_game/Ghost_house_game/obj/Debug/net9.0/Ghost_house_game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ghost_house_game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e23e00e3a6d23249ef8253e52a8088563d1d97a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ghost_house_game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ghost_house_game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
